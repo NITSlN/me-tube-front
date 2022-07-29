b24f21d3fbc66a374a999e25ab1eeae4f128a153
nitish@gaga <nitishgoku01@gmail.com>
2022-07-29 17:14:29 +0530

    api url added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { LoginProvider } from './context/LoginContext'
+axios.defaults.baseURL = 'https://me-tube-backend.herokuapp.com/api';
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
